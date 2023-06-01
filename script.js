f76c3a30cd2414b8131800af9f77bc02b62e42e4
Tiago Alves <tiago.alvessilva67@gmail.com>
2023-06-01 11:04:04 -0300

    Modificando os arquivos

--- script.js
+++ script.js
@@ -1,47 +1,55 @@
-let validator_1 = {
-    handleSubimit: (Event)=>{
-        Event.preventDefault();
+let validator = {
+    handleSubmit:(event)=>{
+        event.preventDefault();
         let send = true;
-        
+
         let inputs = form.querySelectorAll('input');
 
-        for(let i=0; i<inputs.length; i++){
+        for(let i=0;i<inputs.length;i++) {
             let input = inputs[i];
-            let check = validator_1.checkInput(input);
-            if(check !== true){
+            let check = validator.checkInput(input);
+            if(check !== true) {
                 send = false;
                 console.log(check)
-                //exibir o error
             }
         }
 
-        send = false;
-        if(send){
+        if(send) {
             form.submit();
         }
     },
-    checkInput: (input) =>{
+
+    checkInput:(input) => {
         let rules = input.getAttribute('data-rules');
-        
-        if(rules !== null){
+
+        if(rules !== null) {
             rules = rules.split('|');
-            for(let k in rules){
-                
+            for(let k in rules) {
                 let rDetails = rules[k].split('=');
-                
-                switch(rDetails[0]){
+                switch(rDetails[0]) {
                     case 'required':
-                        if(input.value==''){
-                            return 'Campo não pode ser vazio';
+                        if(input.value == '') {
+                            return 'Campo não pode ser vazio.';
                         }
                     break;
                     case 'min':
+                        
+                    break;
+                    /*case 'email':
+                        if(input.value != '') {
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if(!regex.test(input.value.toLowerCase())) {
+                                return 'E-mail digitado não é válido!';
+                            }
+                        }
+                    break; */
                 }
             }
         }
-    }
 
-}
+        return true;
+    },
+};
 
-let form = document.querySelector('.validator_1');
-form.addEventListener('submit', validator_1.handleSubimit)
+let form = document.querySelector('.validator');
+form.addEventListener('submit', validator.handleSubmit)
